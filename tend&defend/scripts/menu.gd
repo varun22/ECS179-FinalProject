f39ad63072720bfaf61eb160333cc5b6ab89d827
simondgooden <sdgooden@ucdavis.edu>
2024-12-08 16:10:43 -0800

    Added background/foreground
    Changed layout slightly

--- tend&defend/scripts/menu.gd
+++ tend&defend/scripts/menu.gd
@@ -2,8 +2,15 @@ class_name Menu
 extends Control
 
 
+func _ready():
+	var window_size = DisplayServer.window_get_size()
+	print("Window dimensions: ", window_size.x, "x", window_size.y)
+
+
+
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/stage1.tscn")
+	
 	
 
 func _on_credits_pressed() -> void:
