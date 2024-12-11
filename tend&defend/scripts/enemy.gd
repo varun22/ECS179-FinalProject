94e9b31864b7abca0b6d7433c44537b16fb797b1
mariposa7273 <75295879+mariposa7273@users.noreply.github.com>
2024-12-11 11:42:45 -0800

    Added enemy animations

--- tend&defend/scripts/enemy.gd
+++ tend&defend/scripts/enemy.gd
@@ -11,6 +11,7 @@ signal died(value: int)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	signals.enemy_damaged.connect(_on_enemy_damaged)
+	$Sprite2D/AnimationPlayer.play("cloak_move_left")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
